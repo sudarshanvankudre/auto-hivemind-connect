fd86d855a2f3a3c8c2eeb36d30d3a7d35f539977
sudarshan <joecurious7@gmail.com>
2020-04-20 21:43:32 -0700

    adding command line argument parsing

--- hive_connect.py
+++ hive_connect.py
@@ -2,7 +2,10 @@ import json
 import urllib.request
 import argparse
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Start ssh session with UC Berkeley hivemind machine")
+parser.add_argument("username", help="Your username. Ex: cs000-aaa")
+parser.add_argument("password", help="Your password corresponding to username")
+args = parser.parse_args()
 data = json.load(urllib.request.urlopen("https://www.ocf.berkeley.edu/~hkn/hivemind/data/latest.json"))
 
 
@@ -14,6 +17,7 @@ def compare_machine(machine):
 
 machines = filter(lambda s: s.startswith("hive") and "load_avgs" in data["data"][s], data["data"])
 lowest_load_machine = min(machines, key=compare_machine)
+
 
 
 
